484baeaf9cf9fc2bc95ecde4cef2aa009d72bcb3
Drew E. Brown <drew.e.brown@protonmail.com>
2023-03-26 13:42:20 -0400

    pdf fix indeedy

--- _source/conf.py
+++ _source/conf.py
@@ -33,4 +33,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static/_assets']
+html_static_path = ['_static', '_assets']
